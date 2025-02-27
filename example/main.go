3a81c5b6961e134af87bd1b84025bbdc00562edb
Marco Muratori <marcomrtr@gmail.com>
2025-02-27 23:27:24 +0100

    chore: fix merge conflicts
    Took 7 minutes

--- example/main.go
+++ example/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"log"
-	"math/rand"
-	"os"
 	"strconv"
 	"time"
 
@@ -15,8 +12,8 @@ import (
 )
 
 const (
-	TRAIN_SAMPLES_PER_DIGIT = 400
-	TEST_SAMPLES_PER_DIGIT  = 40
+	TRAIN_SAMPLES_PER_DIGIT = 1000
+	TEST_SAMPLES_PER_DIGIT  = 100
 
 	progressBarWidth = 60
 )
@@ -31,7 +28,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	testData, err := getData("./mnist/mnist_test.csv", TEST_SAMPLES_PER_DIGIT, true)
+	testData, err := getData("./mnist/mnist_test.csv", TEST_SAMPLES_PER_DIGIT, false)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -122,63 +119,4 @@ func test(
 
 	totalTime := time.Since(startTime)
 	fmt.Printf("Total execution time: %s\n", totalTime.Round(time.Second))
-}
-
-func getData(path string, samplesPerDigit int, shuffle bool) (map[string][][]float64, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open mnist_test.csv: %v", err)
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	data, err := reader.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("failed to read CSV: %v", err)
-	}
-
-	dataset := make(map[string][][]float64)
-
-	for i := range 10 {
-		key := fmt.Sprintf("%d", i)
-		dataset[key] = [][]float64{}
-	}
-
-	for _, row := range data {
-		label := row[0]
-		pixels := make([]float64, len(row)-1)
-		for j, val := range row[1:] {
-			p, err := strconv.ParseFloat(val, 64)
-			if err != nil {
-				return nil, fmt.Errorf("failed to parse float: %v", err)
-			}
-			// normalized values
-			pixels[j] = p / 255
-		}
-
-		currentSamples := dataset[label]
-		if samplesPerDigit == -1 || len(currentSamples) < samplesPerDigit {
-			dataset[label] = append(currentSamples, pixels)
-		} else {
-			continue
-		}
-	}
-
-	// Shuffle samples
-	if shuffle {
-		for key := range dataset {
-			samples := dataset[key]
-			for i := range samples {
-				j := i + rand.Intn(len(samples)-i)
-				samples[i], samples[j] = samples[j], samples[i]
-			}
-			if samplesPerDigit != -1 && len(samples) > samplesPerDigit {
-				dataset[key] = samples[:samplesPerDigit]
-			} else {
-				dataset[key] = samples
-			}
-		}
-	}
-
-	return dataset, nil
 }
